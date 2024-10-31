7a308d51a734176602dea6d180e21672e6440618
Yafiaha <ynadbullah244@gmail.com>
2024-10-31 09:16:22 -0400

    Fix-Up

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -100,15 +100,8 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 //delete the invoice
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to Delete Invoice');
-  try {
-    await sql`DELETE FROM invoices WHERE id = ${id}`;
-    revalidatePath('/dashboard/invoices');
-    return { message: 'Deleted Invoice.' };
-  } catch (error) {
-    return { message: 'Database Error: Failed to Delete Invoice.' };
-  }
-  
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
 
 //login
